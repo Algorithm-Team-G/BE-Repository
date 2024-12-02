c63e2e737726cad171b93d7eefeabde5a1c1941d
kug4586 <kug040202@gmail.com>
2024-12-02 19:11:16 +0900

    [chore] 기타 수정
    - 테이블 생성 명령어 수정
    - Graph.py 주석 수정

--- utils/Graph.py
+++ utils/Graph.py
@@ -56,7 +56,7 @@ class Graph:
         1. T의 마감 날짜가 급한 경우 (S)
         2. T의 중요도가 높은 경우 (B)
         3. T의 작업 기간이 짧은 경우 (S)
-        4. W의 career 경력이 높은 경우 (B)
+        4. W가 닌이도가 level T를 수행하기에 적합한 경우 (B)
         5. W와 T의 직군이 같은 경우 (S)
 
         [점수 계산식]
@@ -72,7 +72,7 @@ class Graph:
         # p3. T의 작업 기간이 짧은 경우 (작은 게 이득)
         p3 = (task.end - task.begin).days * self.weights[2]
 
-        # p4. T의 Level이 W의 career와 적절한 경우 (큰 게 이득)
+        # p4. W가 T를 수행하기에 적합한 경우
         p4 = worker.career / task.level * self.weights[3]
 
         # p5. W와 T의 직군이 같은 경우 (작은 게 이득)
